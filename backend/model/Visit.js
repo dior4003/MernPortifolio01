8691c39f0b567e9a1c0d29253ca758aaa107d99c
dior4003 <78859588+dior4003@users.noreply.github.com>
2025-07-09 01:45:45 +0300

    Create Visit.js

---
+++ backend/model/Visit.js
@@ -0,0 +1,21 @@
+const visitSchema = new mongoose.Schema({
+  ip: String,
+  userAgent: String,
+  path: String,
+  method: String,
+  statusCode: Number,
+  referrer: String,
+  isBot: Boolean,
+  sessionId: String,
+  country: String,
+  city: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  duration: {
+    type: Number, // sekundlarda
+    default: 0,
+  },
+}, { timestamps: true });
