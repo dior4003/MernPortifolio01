0c28d1c5b650b3c0151dd722707f1cc3b890b72a
dior4003 <78859588+dior4003@users.noreply.github.com>
2025-07-10 00:19:25 +0300

    Create visitRoutes.js

---
+++ backend/routes/visitRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  logVisit,
+  getVisitStats,
+  clearVisits,
+} from "../controllers/VisitController.js";
+import { isAuthenticated, isAdmin } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/", logVisit);
+router.get("/", isAuthenticated, isAdmin, getVisitStats);
+router.delete("/", isAuthenticated, isAdmin, clearVisits);
+
+export default router;
