8c3536031b93f886a45f57df3773a4011d1ee711
dior4003 <78859588+dior4003@users.noreply.github.com>
2025-07-10 00:11:57 +0300

    Create aboutRoute.js

---
+++ backend/routes/aboutRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getAbout, updateAbout } from "../controllers/aboutController.js";
+import { isAuthenticated, isAdmin } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.get("/", getAbout);
+router.put("/", isAuthenticated, isAdmin, updateAbout);
+
+export default router;
