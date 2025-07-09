a09d943122fd946b8564b0593637fd1853c8f3c5
dior4003 <78859588+dior4003@users.noreply.github.com>
2025-07-09 14:48:28 +0300

    Update AboutMe.js

--- backend/model/AboutMe.js
+++ backend/model/AboutMe.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
+const itemSchema = new mongoose.Schema({
+  icon: { type: String, required: true },
+  value: { type: String, required: true },
+}, { _id: false });
+
 const aboutMeSchema = new mongoose.Schema({
-  icon: {
-    type: String,
-    required: true,
-  },
-  value: {
-    type: String,
-    required: true,
-  },
+  items: [itemSchema],
 }, {
   timestamps: true,
 });
