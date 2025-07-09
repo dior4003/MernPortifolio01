34885822bae60dbcceb7c0022f7a2078b13667da
dior4003 <78859588+dior4003@users.noreply.github.com>
2025-07-09 13:51:21 +0300

    Update Project.js

--- backend/model/Project.js
+++ backend/model/Project.js
@@ -1,25 +1,12 @@
-import mongoose from "mongoose";
 
-const imageSchema = new mongoose.Schema({
-  public_id: String,
-  url: String,
-}, { _id: false });
-
+export const Project = mongoose.model("Project", projectSchema);
 const projectSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
+  title: { type: String, required: true },
   url: String,
   description: String,
   techStack: String,
-  image: imageSchema,
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
+  image: { type: mongoose.Schema.Types.ObjectId, ref: "Image" },
+  isDeleted: { type: Boolean, default: false },
 }, {
   timestamps: true,
 });
-
-export const Project = mongoose.model("Project", projectSchema);
