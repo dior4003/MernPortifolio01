fd84d9c6a976c13b93c49aa405cce7cafe86d871
dior4003 <78859588+dior4003@users.noreply.github.com>
2025-08-05 00:29:32 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,9 +8,9 @@ export const isAuthenticated = async (req, res, next) => {
     if (!token) return res.status(401).json({ success: false, message: "Token yo‘q" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded) return res.status(401).json({ success: false, message: "Foydalanuvchi topilmadi" });
-    req.user = await User.findById(decoded.id).select("-password");
-    
+    req.user = await User.findById(decoded._id).select("-password");
+    if (!req.user) return res.status(401).json({ success: false, message: "Foydalanuvchi topilmadi" });
+ 
 
     next();
   } catch (err) {
